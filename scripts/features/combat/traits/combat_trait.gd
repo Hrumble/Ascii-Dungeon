9c19dfb576908ab4df7c4730a55d03e40aea07f1
Hrumble <nolan.gero@gmail.com>
2026-03-10 20:45:50 +0100

    feat: Combat traits

---
+++ scripts/features/combat/traits/combat_trait.gd
@@ -0,0 +1,42 @@
+class_name CombatTrait
+## A combat trait is a trait that an entity (opponent) can possess during combat.
+## Whereas the player fights with his equipment, an entity fights with its traits, that, similarly to the equipment reacts to events and actions
+## An entity with the "Blood Lust" trait (example) would attack the player on every event if the player is below 10HP for instance.
+## You can create modular and creative entities by combining multiple traits.
+
+## Bool that handles checking wether or not this item has already reacted to an event during this sequence.
+## Used to avoid looping reactions
+var f_has_reacted : bool = false
+
+## Connects this entity to the fight
+func connect_to_fight(fight : Fight):
+	f_has_reacted = false
+	fight.sequencer.action_resolved.connect(on_action_resolved)
+	fight.sequencer.reactions_done.connect(_on_reactions_done)
+	pass
+
+## What happens when all the reactions have been resolved
+func _on_reactions_done():
+	f_has_reacted = false
+
+## Connects this entity to the fight. 
+## To be overriden
+func _connect_to_fight(_fight : Fight):
+	pass
+
+## What happens when an action gets resolved, used for reactions.
+## Will not run if the action was resolved by `self`, or `f_has_reacted` is true.
+## calls _react_to_action
+func on_action_resolved(action : QueueAction, ctx : FightContext):
+	# ensure we don't react to our own action
+	if action.source == self:
+		return
+	# Ensure we don't react multiple times in a single turn
+	if f_has_reacted:
+		return
+	_react_to_action(action, ctx)
+
+## React to an action being resolved, do not forget to set f_has_reacted to `true` if you want to avoid infinite loops.
+## To be overriden
+func _react_to_action(_action : QueueAction, _ctx : FightContext):
+	pass
--- scripts/features/entities/entity.gd
+++ scripts/features/entities/entity.gd
@@ -29,9 +29,12 @@ signal on_take_damage(damage : float)
 
 ## Is this entity considered dead
 var is_dead : bool = false
+var combat_traits : Array[CombatTrait] = []
 
-## The room in which this entity is currently, if the entity has not been spawned this returns null
+## The room in which this entity currently is, if the entity has not been spawned this returns null
 var _current_room : Room
+const COMBAT_TRAIT_DIR : String = "res://scripts/features/combat/traits/"
+const ENTITY_TYPE_DIR : String = "res://scripts/features/entities/types/"
 
 static func fromJSON(json: String, _id : String) -> Entity:
 	var parsed_json = JSON.parse_string(json)
@@ -46,10 +49,10 @@ static func fromJSON(json: String, _id : String) -> Entity:
 	var entity: Entity
 
 	if type != null:
-		var _path: String = "res://scripts/features/entities/types/%s.gd" % type
+		var _path: String = "%s%s.gd" % [ENTITY_TYPE_DIR, type]
 		if !FileAccess.file_exists(_path):
 			GlobalLogger.log_e(
-				"Failed to create entity, the specifid file does not exist: " + _path
+				"Failed to create entity, the specified file does not exist: " + _path
 			)
 			return null
 		else:
@@ -78,6 +81,22 @@ static func fromJSON(json: String, _id : String) -> Entity:
 	entity.loot_table = parsed_json.get("loot_table", [])
 	entity.base_sp = parsed_json.get("base_sp", 3)
 
+	## Handles combat traits
+	for trait_dict : Dictionary in parsed_json.get("combat_traits", []):
+		var _path : String = "%s%s.gd" % [COMBAT_TRAIT_DIR, trait_dict["trait_name"]]
+		if !FileAccess.file_exists(_path):
+			GlobalLogger.log_e("%s> There is no combat trait at path: %s" % [_id, trait_dict["trait_name"]])
+			continue
+		var combat_trait : CombatTrait = load(_path).new()
+		for key : String in trait_dict.get("trait_parameters", {}).keys():
+			if key in combat_trait:
+				combat_trait.set(key, trait_dict["trait_parameters"][key])
+			else:
+				GlobalLogger.log_w("%s> %s has no property called %s" % [_id, combat_trait, key])
+		entity.combat_traits.append(combat_trait)
+		GlobalLogger.log_d("%s> Added combat trait %s to entity" % [_id, trait_dict["trait_name"]])
+		pass
+	
 	entity.current_health = entity.base_health
 	entity.current_sp = entity.base_sp
 
@@ -87,7 +106,7 @@ static func fromJSON(json: String, _id : String) -> Entity:
 			if key in entity:
 				entity.set(key, type_properties[key])
 			else:
-				GlobalLogger.log_w("ParsingEntity> %s has no property called %s!" % [type, key])
+				GlobalLogger.log_w("%s> %s has no property called %s!" % [_id, type, key])
 
 	return entity
 
